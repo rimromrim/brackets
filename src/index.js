719bf78be3b921aa63bfc619ccdd2cc26a9706d4
Semyon <cemen12532@gmail.com>
2023-01-26 17:24:15 +0500

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let brack = []
+  for (let n = 0; n < bracketsConfig.length; n++) {
+    brack.push(bracketsConfig[n].join(''))
+  }
+  while (brack.find((a) => str.indexOf(a) !== -1)) {
+    for (let i = 0; i < brack.length; i++) {
+      str = str.replaceAll(brack[i], '');
+    }
+  }
+  return str == '';
+};
